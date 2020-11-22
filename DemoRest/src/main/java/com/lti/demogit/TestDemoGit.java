b31a2d059ff03617176a11b3380c7f76927fbe14
Sachin Nihalani <36417253+sachinn319@users.noreply.github.com>
2020-11-22 20:48:50 +0530

    Update TestDemoGit.java

--- DemoRest/src/main/java/com/lti/demogit/TestDemoGit.java
+++ DemoRest/src/main/java/com/lti/demogit/TestDemoGit.java
@@ -3,5 +3,6 @@ package com.lti.demogit;
 public class TestDemoGit {
 	public static void main(String[] args) {
 		System.out.println("Deva re deva");
+		System.out.println("Deva re deva 2 ");
 	}
 }
